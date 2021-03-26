27887b01dcafb4be65d65debe46204358e407c5a
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 16:58:12 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ModificadoresAcesso
+{
+    namespace FiapHelloWorld
+    {
+        public class Curso
+        {
+            #region atributos
+            int Codigo;
+            internal string NomeCurso;
+            public string NomeInstrutor;
+            private int CargaHorario;
+            protected int MinimoAlunos;
+            protected internal int MaximoAlunos;
+            #endregion
+            public Curso()
+            {
+                // Construtor padrão.
+            }
+            protected internal Curso(string nome, string instrutor)
+            {
+                this.NomeCurso = nome;
+                this.NomeInstrutor = instrutor;
+            }
+            private Curso(string nome, int minimo, int maximo)
+            {
+                this.NomeCurso = nome;
+                this.MaximoAlunos = maximo;
+                this.MinimoAlunos = minimo;
+            }
+            public void CriarCurso(string nome, string instrutor)
+            {
+                this.NomeCurso = nome;
+                this.NomeInstrutor = instrutor;
+            }
+            private bool MatricularAluno(string nomeAluno)
+            {
+                // Verificar a quantidade de alunos
+                return true;
+            }
+            private int ConsultarMaximoAlunos()
+            {
+                // Retorno o valor do atributo
+                return this.MaximoAlunos;
+            }
+        }
+    }
+}
